84d20b210aeafd1467fa0ce9a389f3eb176f219c
Felix <f.michaux@rcvs.org.uk>
2016-05-06 21:46:03 +0100

    Rich functionality

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert('Hello world!');
+
+//Set the scene, init some variables for the field  button
+
+var textField = document.getElementsByName('new_item')[0];
+var createTodoButton = document.getElementById('create_todo');
+
+
+//Get value of textField on keyup
+textField.onkeyup = function(){
+  textFieldValue = textField.value;
+};
+
+
+//Action should only be taken upon clicking the button
+createTodoButton.onclick = function() {
+  alert(textFieldValue);
+};
