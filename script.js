6985675f4754f1acbd8929e4c12eb00bdf79c034
Felix <f.michaux@rcvs.org.uk>
2016-05-07 00:42:15 +0100

    now shows most recent todo at top of list

--- script.js
+++ script.js
@@ -4,24 +4,32 @@
 var textField = document.getElementsByName('new_item')[0];
 var createTodoButton = document.getElementById('create_todo');
 
-
 //Get value of textField on keyup
 textField.onkeyup = function(){
   textFieldValue = textField.value;
 };
-
 
 //Function to append textFieldValue into location
 function insertTodo() {
   var li = document.createElement("li");
   var content = document.createTextNode(textFieldValue);
   li.appendChild(content);
-
   var parentElement = document.getElementById('item_list');
   parentElement.appendChild(li);
+}
+
+//Re-arrange
+function bottomToTop() {
+  //Sort most recent at top
+  var todoCount = document.getElementById('item_list').childElementCount;
+  var todoItems = document.getElementById('item_list').children;
+  var lastTodo = todoItems[todoCount-1];
+  var parentElement = document.getElementById('item_list');
+  parentElement.insertBefore(lastTodo, todoItems[0]);
 }
 
 //Action should only be taken upon clicking the button
 createTodoButton.onclick = function() {
   insertTodo();
+  bottomToTop();
 };
