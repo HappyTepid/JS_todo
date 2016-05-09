56dd9e2af87cdc8f042ee153ac5136bfdf087b78
Felix <f.michaux@rcvs.org.uk>
2016-05-09 21:52:35 +0100

    Linted/Hinted

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 //Variables:
 var textField = document.getElementsByName('new_item')[0];
 var createTodoButton = document.getElementById('create_todo');
@@ -6,50 +5,59 @@ var sortButton = document.getElementById('reverse_sort');
 var todoTally = 0;
 
 //Functions:
-//Function to append textFieldValue into location
-function insertTodo() {
-  var li = document.createElement("li");
-  var checkbox = document.createElement("INPUT");
-  checkbox.setAttribute("type", "checkbox");
-  var checkboxID = todoTally+1;
-  checkbox.setAttribute("id", checkboxID);
-  var content = document.createTextNode(' '+textField.value);
-  li.appendChild(checkbox);
-  li.appendChild(content);
-  var parentElement = document.getElementById('item_list');
-  parentElement.appendChild(li);
-  todoTally++;
-  armCheckbox();
-}
 //Give those checkboxes a purpose
 function armCheckbox() {
-  var boxNo = todoTally;
-  var checkbox = document.getElementById(boxNo);
-  checkbox.onclick = function() {
+  var checkbox = document.getElementById(todoTally.toString());
+  checkbox.onclick = function () {
     if (checkbox.checked) {
       checkbox.parentElement.style.textDecoration = 'line-through';
-    }
-    else {
+    } else {
       checkbox.parentElement.style.textDecoration = '';
     }
   };
 }
+//Function to append textFieldValue into location
+function insertTodo() {
+  var li = document.createElement("li"),
+    checkbox = document.createElement("INPUT"),
+    checkboxID = todoTally + 1,
+    content = document.createTextNode(' ' + textField.value),
+    parentElement = document.getElementById('item_list');
+  //var checkbox = document.createElement("INPUT");
+  checkbox.setAttribute("type", "checkbox");
+  //var checkboxID = todoTally+1;
+  checkbox.setAttribute("id", checkboxID.toString());
+  //var content = document.createTextNode(' '+textField.value);
+  li.appendChild(checkbox);
+  li.appendChild(content);
+  //var parentElement = document.getElementById('item_list');
+  parentElement.appendChild(li);
+  todoTally++;
+  armCheckbox();
+}
 //Re-arrange
 function bottomToTop() {
   //Sort most recent at top
-  var todo = document.getElementById('item_list');
-  var todoCount = todo.childElementCount;
-  var todoItems = todo.children;
-  var lastTodo = todoItems[todoCount-1];
+  var todo = document.getElementById('item_list'),
+    todoCount = todo.childElementCount,
+    todoItems = todo.children,
+    lastTodo = todoItems[todoCount - 1];
+  //var todoCount = todo.childElementCount;
+  //var todoItems = todo.children;
+  //var lastTodo = todoItems[todoCount-1];
   todo.insertBefore(lastTodo, todoItems[0]);
 }
 //Reverse sort order (oldest first/newest first)
 function reverseSort() {
-  var todo = document.getElementById('item_list');
-  var todoCount = todo.childElementCount;
+  var todo = document.getElementById('item_list'),
+    todoCount = todo.childElementCount,
+    i,
+    todoItems,
+    lastTodo;
+  //var todoCount = todo.childElementCount;
   for (i = 0; i < todoCount; i++) {
-    var todoItems = todo.children;
-    var lastTodo = todoItems[todoCount-1];
+    todoItems = todo.children;
+    lastTodo = todoItems[todoCount - 1];
     todo.insertBefore(lastTodo, todoItems[i]);
   }
 }
@@ -64,16 +72,16 @@ function createTodo() {
 
 //Respond to input:
 //Act on keyboard enter press
-textField.onkeyup = function(){
-  if (event.keyCode == 13) {
-     event.preventDefault();
-     createTodo();
+textField.onkeyup = function () {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    createTodo();
   }
 };
 //Action should only be taken upon clicking the button
-createTodoButton.onclick = function() {
+createTodoButton.onclick = function () {
   createTodo();
 };
-sortButton.onclick = function() {
+sortButton.onclick = function () {
   reverseSort();
 };
